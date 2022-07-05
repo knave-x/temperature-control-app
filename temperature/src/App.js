0a9c838ee1b51def46d1fd8629f21609f1575136
batuhan aydin <baydin@nuevo.com.tr>
2022-07-05 13:16:33 +0300

    features added

--- temperature/src/App.js
+++ temperature/src/App.js
@@ -1,16 +1,40 @@
 import "./App.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    const newTemperature = temperatureValue + 1;
+
+    if (newTemperature >= 15) {
+      setTemperatureColor(`hot`);
+    }
+
+    setTemperatureValue(newTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    const newTemperature = temperatureValue - 1;
+
+    if (newTemperature < 15) {
+      setTemperatureColor(`cold`);
+    }
+
+    setTemperatureValue(newTemperature);
+  };
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display"> 10°C</div>
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
       </div>
       <div className="button-container">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => increaseTemperature()}>+</button>
+        <button onClick={() => decreaseTemperature()}>-</button>
       </div>
     </div>
   );
