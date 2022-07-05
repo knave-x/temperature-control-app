29c23b201d27bb349371441793e1c9176d4cedff
batuhan aydin <baydin@nuevo.com.tr>
2022-07-05 12:54:18 +0300

    temperature control app

---
+++ temperature/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display"> 10°C</div>
+      </div>
+      <div className="button-container">
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
